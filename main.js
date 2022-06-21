61663ba825a7b86fdae9a67a910cf017ad44a063
Damilare Oyetade <damilareoyetade1@gmail.com>
2022-06-21 16:08:46 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+var firstname = 'Damilare';
+var lastname = 'Oyetade';
+var height = "5'9ft";
+var country = 'Nigeria';
+
+document.write('Hi Mentor, I am ' + firstname + ' ' + lastname + ' and my height is ' + height + "<br>" + ' I am from ' + country);
